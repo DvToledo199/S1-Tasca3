1229d02c8c2eb5c6f36911304f6b186083a70705
David Toledo <toledo_1990@hotmail.com>
2026-02-14 20:19:34 +0100

    feat: setup level 2 exercise 2 based on exercise 1

---
+++ src/level2/exercise2/Main.java
@@ -0,0 +1,22 @@
+package level2.exercise2;
+
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args){
+
+        HashSet<Restaurant> restaurants = new HashSet<>();
+
+        Restaurant r1 = new Restaurant("Pizza Roma", 8);
+        Restaurant r2 = new Restaurant("Pizza Roma", 8);
+        Restaurant r3 = new Restaurant("Pizza Roma", 9);
+        Restaurant r4 = new Restaurant("Burger King", 8);
+
+        restaurants.add(r1);
+        restaurants.add(r2);
+        restaurants.add(r3);
+        restaurants.add(r4);
+
+        System.out.println(restaurants);
+    }
+}
---
+++ src/level2/exercise2/Restaurant.java
@@ -0,0 +1,36 @@
+package level2.exercise2;
+import java.util.Objects;
+
+public class Restaurant {
+    private String name;
+    private int score;
+
+    public Restaurant(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj){
+            return true;
+        }
+        if(obj == null) {
+            return false;
+        }
+        if (this.getClass() != obj.getClass()){
+            return false;
+        }
+        Restaurant other = (Restaurant) obj;
+        return Objects.equals(this.name, other.name) && this.score == other.score;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, score);
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + score;
+    }
+}
